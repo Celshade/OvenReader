b09a0b13704ee8de908c07cedf2d7750ba0d0192
Danny Collins | Celshade <ggCelshade@gmail.com>
2020-03-25 16:02:56 -0400

    Cook.py documentation update & attr config

--- ovenreader/cook.py
+++ ovenreader/cook.py
@@ -2,37 +2,68 @@
 class Cook(object):
     """Maintain parsed cook data.
 
-    Intended to be initialized with a dictionary of attributes, which
-    are then unpacked. This object is intended to be a container for
-    unique 'cook' data
+    This object is intended to be used in conjunction with OvenReader() and
+    maintains unique cook data. Attributes are specified in a pre-configured
+    dict.
+
+    Args:
+        data: Configruation of attributes.
 
     Attributes:
-        data: A dictionary of attributes and their values.
+        file_name (str): File name for the cook.
+        product (str): Product ID.
+        lot (str): Product lot number.
+        in_weight (int): Product in-weight.
+        out_weight (int): Product out-weight.
+        oven_yield (float): Product yield.
+        oven (str): Oven ID.
+        program (str): Program name.
+        start_time (str): Cook start time.
+        end_time (str): Cook end time.
+        duration (str): Total duration of cook.
+        stages (dict): Stage information.
+        comments (list): Cook comments.
+        errors (list): Cook errors.
     """
 
-    def __init__(self, data: dict=None) -> None:
-        self._fpath = None
-        self._in_weight = None
-        self._out_weight = None
-        self._lot = None
-        self._product = None
-        self._oven = None
-        self._program = None
-        self._start_time = None
-        self._end_time = None
-        self._duration = None
-        self._yield = None
-        self._stages = {}  # dict containing stage data
-        # TODO implement comments and errors
-        # self._comments = []  # list of tules containing comment data
-        # self._errors = []  # list of tuples containing error data
-
-        # TODO unpack data (dict) into attributes
-        # raise NotImplementedError("Class not implemented.")
-
-        print(self.__dict__.items())
+    def __init__(self, data: dict) -> None:
+        self.file_name = data["fname"]
+        self.product = data["product"]
+        self.lot = data["lot"]
+        self.in_weight = data["in_weight"]
+        self.out_weight = data["out_weight"]
+        self.cook_yield = data["yield"]
+        self.oven = data["oven"]
+        self.program = data["program"]
+        self.start_time = data["start_time"]
+        self.end_time = data["end_time"]
+        self.duration = data["duration"]
+        self.stages = data["stages"]
+        self.comments = data["comments"]
+        self.errors = data["errors"]
 
 
 if __name__ == "__main__":
     # TODO remove tests
-    test = Cook()
+    TEST_CONFIG = {
+        "fname": ".\\path",
+        "product": "404E",
+        "lot": "PL123456L",
+        "in_weight": 420,
+        "out_weight": 110,
+        "yield": .50,
+        "oven": "V5",
+        "program": '3',
+        "start_time": "1/1/2020 00:00",
+        "end_time": "1/2/20 00:00",
+        "duration": 560,
+        "stages": {},
+        "comments": [],
+        "errors": []
+    }
+    try:
+        test = Cook(TEST_CONFIG)
+    except KeyError:
+        print("\nConfiguration failed...")
+        print("Deleting object...")
+    print(help(Cook))
